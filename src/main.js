4e5c374435b35633e99a3efd88f6f3c057760526
konmike <michalkonecny10@gmail.com>
2021-06-25 15:27:13 +0200

    Move style from App and Box to scss folder

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import mitt from "mitt";
 
+import "./assets/scss/main.scss";
+
 const emitter = mitt();
 
 const app = createApp(App);
