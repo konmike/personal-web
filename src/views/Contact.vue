0971268286215893bfa704c225d231c6c631fd71
konmike <michalkonecny10@gmail.com>
2022-01-06 15:14:39 +0100

    Add Contact page

---
+++ src/views/Contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="contact">
+    <h1 class="title">Máte zájem<br />o spolupráci?</h1>
+    <h3 class="email">info@konecnymichal.cz</h3>
+    <div class="links">
+      <a href="https://www.linkedin.com/in/michal-konecny/"
+        ><img class="icon" src="../assets/linkedin.png" alt="Go to my Linkedin"
+      /></a>
+      <a href="https://github.com/konmike/"
+        ><img
+          class="icon"
+          src="../assets/github/GitHub-Mark-Light-120px-plus.png"
+          alt="Go to my Github"
+      /></a>
+    </div>
+  </div>
+</template>
