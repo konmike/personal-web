cfe1277092825cdb191a945e03db01b87d06f22e
konmike <michalkonecny10@gmail.com>
2022-01-07 09:46:01 +0100

    Add projects close component

---
+++ src/components/ProjectsClose.vue
@@ -0,0 +1,13 @@
+<template>
+  <span class="close" @click="closeProjects">Zavřít</span>
+</template>
+
+<script>
+export default {
+  methods: {
+    closeProjects() {
+      this.emitter.emit("show-projects", false);
+    },
+  },
+};
+</script>
